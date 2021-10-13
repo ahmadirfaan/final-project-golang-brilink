243783b6de381f59ae4618c6e8a6ff683d4b90a1
wahyuningtiyas <wtyas4501@gmail.com>
2021-10-13 11:13:50 +0700

    district agent

--- models/database/districts.go
+++ models/database/districts.go
@@ -1,7 +1,8 @@
 package database
 
 type Districts struct {
-	Id        string `json:"id" gorm:"type:char(7)"`
-	RegencyId string `json:"regencyId"`
-	Name      string `json:"name"`
+	Id        string  `json:"id" gorm:"type:char(7)"`
+	RegencyId string  `json:"regencyId"`
+	Name      string  `json:"name"`
+	Agent     []Agent `gorm:"Foreignkey:DistrictId;association_foreignkey:ID;" json:"agent"`
 }
