d069f6b126d25228bf382131183446c322351ad8
ahmadirfaan <irfaan.hibatullah@gmail.com>
2021-10-15 11:32:33 +0700

    edit manifest workflow github

--- vendors/indonesia.sql
+++ vendors/indonesia.sql
@@ -8,11 +8,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-USE brilink;
 --
 -- Table structure for table `provinces`
 --
+
+USE backend-b-antar-jemput;
 
 DROP TABLE IF EXISTS `provinces`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
