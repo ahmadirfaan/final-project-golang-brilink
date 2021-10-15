00a8b143157bd808abb00ea19e658a1f7870969a
ahmadirfaan <irfaan.hibatullah@gmail.com>
2021-10-15 13:36:32 +0700

    Replacing sql data

--- vendors/role.sql
+++ vendors/role.sql
@@ -1,5 +1,5 @@
 -- Pemilihan database untuk dimasukkan ke dalam role ---
-use brilink;
+use `backend-b-antar-jemput`;
 
 INSERT INTO roles (created_at, updated_at, role) values (current_time, current_time, "Agent");
 INSERT INTO roles (created_at, updated_at, role) values (current_time, current_time, "Customer");
